06d1b28837cbd0d431da28ab7b9beb72d27c0dd9
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2024-02-03 21:23:13 +0300

    created CRUD operations for the api template

---
+++ API-CRUD-Template/API-CRUD-Template/Controllers/FirstEntityController.cs
@@ -0,0 +1,73 @@
+﻿using API_CRUD_Template.Data.Interfaces;
+using API_CRUD_Template.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_CRUD_Template.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstEntityController : ControllerBase
+    {
+        private readonly IFirstEntity _firstEntityService;
+
+        public FirstEntityController(IFirstEntity firstEntityService)
+        {
+            _firstEntityService = firstEntityService ?? throw new ArgumentNullException(nameof(firstEntityService));
+        }
+
+        // GET: api/FirstEntity
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FirstEntity>>> GetAll()
+        {
+            var entities = await _firstEntityService.GetAll();
+            return Ok(entities);
+        }
+
+        // GET: api/FirstEntity/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FirstEntity>> GetById(int id)
+        {
+            var entity = await _firstEntityService.GetById(id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entity);
+        }
+
+        // POST: api/FirstEntity
+        [HttpPost]
+        public async Task<ActionResult<FirstEntity>> Post(FirstEntity firstEntity)
+        {
+            var createdEntity = await _firstEntityService.Post(firstEntity);
+            return CreatedAtAction(nameof(GetById), new { id = createdEntity.Id }, createdEntity);
+        }
+
+        // PUT: api/FirstEntity/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, FirstEntity firstEntity)
+        {
+            var updatedEntity = await _firstEntityService.Update(id, firstEntity);
+
+            if (updatedEntity == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/FirstEntity/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _firstEntityService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ API-CRUD-Template/API-CRUD-Template/Data/Interfaces/IFirstEntity.cs
@@ -0,0 +1,13 @@
+﻿using API_CRUD_Template.Models;
+
+namespace API_CRUD_Template.Data.Interfaces
+{
+    public interface IFirstEntity
+    {
+        Task<List<FirstEntity>> GetAll();
+        Task<FirstEntity> GetById(int id);
+        Task<FirstEntity> Post(FirstEntity firstEntity);
+        Task<FirstEntity> Update(int id, FirstEntity firstEntity);
+        Task Delete(int id);
+    }
+}
